88b6a46dc4023af2ed629bf8ebc83b02572fd008
Allen Shaw <allen@JoineryHQ.com>
2020-02-16 13:01:31 -0600

    Improved decrementing of participant counters.

--- js/CRM_Event_Form_Registration_Confirm-decrement-counters.js
+++ js/CRM_Event_Form_Registration_Confirm-decrement-counters.js
@@ -1,14 +1,28 @@
+/**
+ * File should only be included when isRegisteringSelf = false, so it will decrement
+ * all participant counters by one.
+ */
+
 (function(ts) {
   CRM.$(function($) {
-    $('div.event_fees-group > strong').each(function(idx, el){
-      var counter = $(el).html().replace(/^Participant ([0-9]+)$/, '$1');
+    // Function to alter given html string, decrementing participant count by 1.
+    var decrementParticipant = function decrementParticipant(html) {
+      var counter = html.replace(/^Participant ([0-9]+)$/, '$1');
       if (counter == 1) {
-        $(el).html(ts('Registrant'));
+        // If this was originally 'participant 1', change it to 'Registrant'
+        html = ts('Registrant');
       }
       else {
+        // Otherwise, just decrement by one.
         --counter;
-        $(el).html(ts('Participant') + ' ' + counter);
+        html = ts('Participant') + ' ' + counter;
       }
+      return html;
+    };
+
+    $('div.event_fees-group > strong, div.participant_info-group div.header-dark').each(function(idx, el){
+      var html = decrementParticipant($(el).html().trim());
+      $(el).html(html);
     });
   });
 }(CRM.ts('groupreg')));
