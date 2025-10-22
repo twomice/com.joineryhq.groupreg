79d4cadacd73d166d6d86783858c7512faf759d0
colemanw <coleman@civicrm.org>
2025-10-22 08:10:47 -0400

    Remove calls to deprecated CRM_Utils_Array::value
    Replaces deprecated function with equivalent null-coalescing operator. Behavior should be the same before/after.

--- CRM/Groupreg/BAO/GroupregPriceField.php
+++ CRM/Groupreg/BAO/GroupregPriceField.php
@@ -14,7 +14,7 @@ class CRM_Groupreg_BAO_GroupregPriceField extends CRM_Groupreg_DAO_GroupregPrice
   //   $entityName = 'GroupregPriceField';
   //   $hook = empty($params['id']) ? 'create' : 'edit';
 
-  //   CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
+  //   CRM_Utils_Hook::pre($hook, $entityName, $params['id'] ?? NULL, $params);
   //   $instance = new $className();
   //   $instance->copyValues($params);
   //   $instance->save();
