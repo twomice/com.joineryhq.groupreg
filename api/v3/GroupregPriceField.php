cb1ff34fd719a786e0778f2b88bd4afef582cd6e
colemanw <coleman@civicrm.org>
2025-10-22 20:20:37 -0400

    Replace reference to deprecated exception class
    The api-specific exception classes have been deprecated and will soon be removed from core.
    They are identical to CRM_Core_Exception.

--- api/v3/GroupregPriceField.php
+++ api/v3/GroupregPriceField.php
@@ -21,7 +21,7 @@ function _civicrm_api3_groupreg_price_field_create_spec(&$spec) {
  * @return array
  *   API result descriptor
  *
- * @throws API_Exception
+ * @throws CRM_Core_Exception
  */
 function civicrm_api3_groupreg_price_field_create($params) {
   return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params, 'GroupregPriceField');
@@ -35,7 +35,7 @@ function civicrm_api3_groupreg_price_field_create($params) {
  * @return array
  *   API result descriptor
  *
- * @throws API_Exception
+ * @throws CRM_Core_Exception
  */
 function civicrm_api3_groupreg_price_field_delete($params) {
   return _civicrm_api3_basic_delete(_civicrm_api3_get_BAO(__FUNCTION__), $params);
@@ -49,7 +49,7 @@ function civicrm_api3_groupreg_price_field_delete($params) {
  * @return array
  *   API result descriptor
  *
- * @throws API_Exception
+ * @throws CRM_Core_Exception
  */
 function civicrm_api3_groupreg_price_field_get($params) {
   return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params, FALSE, 'GroupregPriceField');
